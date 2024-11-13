bda7ed6eef30cc22d1614971168ba5f730067bbe
SumaculubT-bu-glue-si <sumaculub_t@bu.glue-si.com>
2024-11-13 14:48:54 +0800

    initial commit

--- src/main/java/com/example/UserManagementSystem/UserManagementSystemApplication.java
+++ src/main/java/com/example/UserManagementSystem/UserManagementSystemApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class  UserManagementSystemApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(UserManagementSystemApplication.class, args);
 	}
 
--- src/test/java/com/example/UserManagementSystem/UserManagementSystemApplicationTests.java
+++ src/test/java/com/example/UserManagementSystem/UserManagementSystemApplicationTests.java
@@ -1,13 +1,20 @@
 package com.example.UserManagementSystem;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
-class UserManagementSystemApplicationTests {
+@AutoConfigureMockMvc
+public class UserManagementSystemApplicationTests {
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+		// test logic here
 	}
-
 }
+
